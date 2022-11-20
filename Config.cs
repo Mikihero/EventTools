8513fda6f661eed22661f5fc528ee240c2a79bf6
Mikihero <mikolajmelaniuk@gmail.com>
2022-11-20 18:30:32 +0100

    EventNow can now also send webhooks

--- Config.cs
+++ Config.cs
@@ -53,11 +53,11 @@ namespace EventTools
         [Description("The message to be broadcasted when using the EventNext command, can be formatted like a normal SL broadcast. {EVENTNAME} will be replaced with the name of the event.")]
         public string ENextBC { get; set; } = "<size=40><b><color=#cc9900>In the next round there will be an event:\n</color></b></size><size=50><b><color=#ff0000>{EVENTNAME}</color></b></size>";
 
-        [Description("Whether or not the EventNext command should send a message to discord, if set to true the ENDiscordWebhookURL value has to be correctly specified. Default: false.")]
+        [Description("Whether or not the EventNext command should send a message to discord, if set to true the ENextDiscordWebhookURL value has to be correctly specified. Default: false.")]
         public bool ENextSendToDiscord { get; set; } = false;
 
         [Description("The message to be sent via a discord webhook when using the EventNext command. Can be formatted like a normal discord message.")]
-        public string ENextDiscordMessage { get; set; } = "In the next round there will be an event: **{EVENTNAME}**";
+        public string ENextDiscordMessage { get; set; } = "In the next round there will be an event: `{EVENTNAME}`";
 
         [Description("Discord ID of the role that should be pinged when using the EventNext command. Leave empty to not ping any role.")]
         public string ENextDiscordRoleID { get; set; } = "";
@@ -73,6 +73,24 @@ namespace EventTools
 
         [Description("The messsage to be broadcasted when using the EventNow command, can be formatted like a normal SL broadcast. {EVENTNAME} will be replaced with the name of the event.")]
         public string ENowBC { get; set; } = "<size=40><b><color=#cc9900>Event:\n</color></b></size> <size=50><b><color=#ff0000>{EVENTNAME}</color></b></size>";
+
+        [Description("Whether or not the EventNow command should send a message to discord, if set to true the  value has to be correctly specified. Default: false.")]
+        public bool ENowSendToDiscord { get; set; } = false;
+
+        [Description("The message to be sent via a discord webhook when using the EventNow command. Can be formatted like a normal discord message.")]
+        public string ENowDiscordMessage { get; set; } = "An event is happening this round: `{EVENTNAME}`";
+
+        [Description("Discord ID of the role that should be pinged when using the EventNow command. Leave empty to not ping any role.")]
+        public string ENowDiscordRoleID { get; set; } = "";
+
+        [Description("Name of the Webhook used in the EventNow command that's displayed on discord.")]
+        public string ENowWebhookName { get; set; } = "EventNotifier";
+
+        [Description("The Discord Webhook URL via which the EventNow command will send messages.")]
+        public string ENowDiscordWebhookURL { get; set; } = "";
+
+        [Description("The avatar Discord Webhook's avatar URL for the EventNow command.")]
+        public string ENowWebhookAvatarURL { get; set; } = "https://media.giphy.com/media/jzKb8n8n2GC6s0cUB1/giphy.gif";
 
         [Description("The broadcast to be sent whenever the lottery command is used. [NUMBER] will be replaced with the chosen number.")]
         public string LotteryBroadcast { get; set; } = "<color=#b00b69><b><size=60>[NUMBER]</size></b></color>";
