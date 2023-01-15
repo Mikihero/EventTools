4c7c00c452ef2697582f40e10c6d6cd9b6864cb5
Mikihero <mikolajmelaniuk@gmail.com>
2023-01-15 22:12:52 +0100

    improved the command

--- commands/EventPrep.cs
+++ commands/EventPrep.cs
@@ -3,14 +3,12 @@ using System.Linq;
 using CommandSystem;
 using Exiled.API.Enums;
 using Exiled.API.Features;
+using Exiled.API.Features.Pickups;
 using Exiled.Permissions.Extensions;
-using InventorySystem.Items.Pickups;
 using LightContainmentZoneDecontamination;
-using Mirror;
 using PlayerRoles;
 using PlayerRoles.FirstPersonControl;
-using UnityEngine;
-using Object = UnityEngine.Object;
+using Map = PluginAPI.Core.Map;
 
 namespace EventTools.Commands
 {
@@ -23,8 +21,8 @@ namespace EventTools.Commands
 
         public string Description => "Allows for easy event preparation.";
 
-        public static bool IsEventActive;
-
+        public static bool IsEventActive = false;
+        
         public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
         {
             if (!Player.Get(sender).CheckPermission("et.eprep"))
@@ -32,31 +30,29 @@ namespace EventTools.Commands
                 response = "You don't have permission to use this command.";
                 return false;
             }
-            if (Plugin.Instance.Config.EPCleanupRagdolls)
+
+            if (Plugin.Instance.Config.EPCloseDoors)
             {
-                BasicRagdoll[] array = (from r in Object.FindObjectsOfType<BasicRagdoll>() orderby r.Info.CreationTime descending select r).ToArray();
-                foreach (var t in array)
+                foreach (Door door in Door.List)
                 {
-                    NetworkServer.Destroy(t.gameObject);
+                    door.IsOpen = false;
                 }
             }
-            if (Plugin.Instance.Config.EPCleanupItems)
-            {
-                ItemPickupBase[] array = Object.FindObjectsOfType<ItemPickupBase>();
-                foreach (ItemPickupBase item in array)
-                {
-                    NetworkServer.Destroy(item.gameObject);
-                }
-            }
+            
             if (Plugin.Instance.Config.EPRoundLock)
             {
                 Round.IsLocked = true;
             }
-            if (Plugin.Instance.Config.EPRespawnTickets)
+            
+            if (Plugin.Instance.Config.EPLockClassD)
             {
-                Respawn.NtfTickets = 1;
-                Respawn.ChaosTickets = 1;
+                foreach (Door door in Door.List.Where(x => x.Type == DoorType.PrisonDoor))
+                {
+                    door.ChangeLock(DoorLockType.AdminCommand);
+                    door.IsOpen = false;
+                }
             }
+            
             if (Plugin.Instance.Config.EPForceClassEveryone)
             {
                 foreach (Player player in Player.List)
@@ -65,30 +61,31 @@ namespace EventTools.Commands
                     player.Role.Set(RoleTypeId.ClassD);
                 }
             }
+            
             if (Plugin.Instance.Config.EPFCToTutorial)
             {
                 Player.Get(sender).Role.Set(RoleTypeId.Tutorial);
             }
-            if (Plugin.Instance.Config.EPTpToTower)
+            
+            if (Plugin.Instance.Config.EPCleanup)
             {
-                Player.Get(sender).Teleport(new Vector3(38, 1014, -31));
+                foreach (Ragdoll ragdoll in Ragdoll.List.ToList()) 
+                    ragdoll.Destroy();
+                foreach (Pickup pickup in Pickup.List.ToList()) 
+                    pickup.Destroy();
             }
-            if (Plugin.Instance.Config.EPLockClassD)
-            {
-                foreach (Door door in Door.List.Where(x => x.Type == DoorType.PrisonDoor))
-                {
-                    door.ChangeLock(DoorLockType.AdminCommand);
-                }
-            }
+            
             if (Plugin.Instance.Config.EPEnableNoclip)
             {
-                FpcNoclip.PermitPlayer(Player.Get(sender).ReferenceHub);
+                Player.Get(sender).IsNoclipPermitted = true;
             }
 
             if (Plugin.Instance.Config.EPDisableDecont)
             {
                 DecontaminationController.Singleton.NetworkDecontaminationOverride = DecontaminationController.DecontaminationStatus.Disabled;
             }
+
+            IsEventActive = true;
             response = "Successfully executed the command.";
             return true;
         }
