f7e7c3b2f51a080e62e9a6971f2670606b1929a4
Mikihero <mikolajmelaniuk@gmail.com>
2023-01-15 22:14:34 +0100

    changed the door and small code improvements

--- commands/EventExplode.cs
+++ commands/EventExplode.cs
@@ -13,18 +13,18 @@ namespace EventTools.Commands
 
         public string[] Aliases { get; set; } = { "eexplode", "eboom", "enuke" };
 
-        public string Description => "Ends the event (and round) with a bang!";
+        public string Description => "Ends the event (and the round) with a bang!";
 
         public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
         {
-            if (!Permissions.CheckPermission(Player.Get(sender), "et.eexplode"))
+            if (!Player.Get(sender).CheckPermission("et.eexplode"))
             {
                 response = "You don't have permission to use this command.";
                 return false;
             }
             foreach (Player player in Player.List)
             {
-                player.Teleport(Door.Get(DoorType.Scp106Bottom));
+                player.Teleport(Door.Get(DoorType.GateA));
                 player.IsGodModeEnabled = false;
             }
             Round.IsLocked = false;
